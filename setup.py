5c9af6252171350e552792ac1222ba520d096f48
Nangallage Dinuwan Thisara Fernando <155742611+dinuwanfernando@users.noreply.github.com>
2024-05-09 17:40:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='loan_prediction_gui',
+    version='1.0',
+    author='Your Name',
+    description='A GUI application for predicting loan status.',
+    packages=find_packages(),
+    py_modules=['loan_prediction_version2'],
+    install_requires=[
+        'pandas',
+        'scikit-learn',
+        # Add any other dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'loan_prediction_version2=loan_prediction_version2:main'
+        ]
+    },
+)
